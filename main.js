5e1c602b1dc470157e8581604b08a0dcb379567e
naazpreetgiani <124805700+naazpreetgiani@users.noreply.github.com>
2023-12-03 22:47:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+// Click Detect Challenge
+
+//Canvas Setup
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+let circles = [];
+for (let n = 1; n <= 10; n++) {
+    circles.push(randomCircle());
+}
+
+let rectangles = [];
+for (let n = 1; n <= 10; n++) {
+    rectangles.push(randomRectangle());
+}
+
+window.addEventListener("load", draw)
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+   for (let i = 0; i < circles.length; i++) {
+       moveCircle(circles[i]);
+       drawCircle(circles[i]);
+    }
+  
+    for (let i = 0; i < rectangles.length; i++) {
+        moveRectangle(rectangles[i]);
+        drawRectangle(rectangles[i]);
+     }
+ requestAnimationFrame(draw);
+}
+
+//Circle Stuff
+
+function drawCircle(aCircle) {
+ ctx.lineWidth = 3;   
+ ctx.strokeStyle = "green";
+ ctx.beginPath();
+ ctx.arc(aCircle.x, aCircle.y, aCircle.r, 0, 2 * Math.PI)
+ ctx.stroke();
+}
+
+function moveCircle(aCircle) {
+    aCircle.y += aCircle.ys;
+    aCircle.x += aCircle.xs;
+
+    //Check for collisions with canvas boundaries
+    if (aCircle.x - aCircle.r < 0 || aCircle.x + aCircle.r > cnv.width) {
+        aCircle.xs = -aCircle.xs;
+    }
+
+    if (aCircle.y - aCircle.r < 0 || aCircle.y + aCircle.r > cnv.height) {
+        aCircle.ys = -aCircle.ys;
+    }
+}
+
+
+function randomCircle() {
+   return {
+        x: randomInt(0, cnv.width),
+        y: randomInt(0, cnv.height),
+        r: randomInt(10, 50),
+        xs: randomInt(1, 5),
+        ys: randomInt(1, 5)
+    }
+}
+
+//Rectangle Stuff
+
+function drawRectangle(aRectangle) { 
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(aRectangle.x, aRectangle.y, aRectangle.sx, aRectangle.sy);
+}
+   
+function moveRectangle(aRectangle) {
+    aRectangle.y += aRectangle.ys;
+    aRectangle.x += aRectangle.xs;
+   
+    //Check for collisions with canvas boundaries
+    if (aRectangle.y > cnv.height || aRectangle.y < 0) {
+        aRectangle.y = randomInt(1, cnv.height);
+    }
+
+    if (aRectangle.x > cnv.width || aRectangle.x < 0) {
+        aRectangle.x = randomInt(1, cnv.width)
+    }
+}
+
+function randomRectangle() {
+    return {
+        x: randomInt(0, cnv.width),
+        y: randomInt(0, cnv.height),
+        r: randomInt(10, 50),
+        xs: randomInt(1, 5),
+        ys: randomInt(1, 5),
+        sx: randomInt(10, 50),
+        sy: randomInt(10, 50)
+    }
+}
